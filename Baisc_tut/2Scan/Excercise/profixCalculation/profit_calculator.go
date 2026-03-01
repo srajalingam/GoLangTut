4461b6c4dffdba0157c207ef9690d4aa074be873
srajalingam <rajalingam299@gmail.com>
2026-03-01 18:49:50 +0530

    Profitcalculator using scan

---
+++ Baisc_tut/2Scan/Excercise/profixCalculation/profit_calculator.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Print("Revenue :")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Expenses :")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Tax Rate :")
+	fmt.Scan(&taxRate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+
+	fmt.Println("EBT:", ebt)
+	fmt.Println("Profit:", profit)
+	fmt.Println("Ratio:", ratio)
+}
