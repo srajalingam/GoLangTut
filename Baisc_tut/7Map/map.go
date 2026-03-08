937ed561fba4df600db610a13fceabae1001081a
srajalingam <rajalingam299@gmail.com>
2026-03-09 00:27:26 +0530

    create map using make

--- Baisc_tut/7Map/map.go
+++ Baisc_tut/7Map/map.go
@@ -22,4 +22,20 @@ func main() {
 	// deleting a key value pair from map
 	delete(websites, "twitter")
 	fmt.Println(websites)
+
+	// make function is used to create a map
+
+	usernames := make(map[string]string)
+	usernames["john"] = "john123"
+	usernames["jane"] = "jane123"
+	fmt.Println(usernames)
+
+	scores := make([]string, 2, 4) // make function is used to allocate memory for slice
+	scores[0] = "100"
+	scores[1] = "90"
+	scores = append(scores, "80") // append function is used to add new element to slice
+	scores = append(scores, "70")
+	scores = append(scores, "60") // when we append more than the capacity of the slice, it will create a new slice with double the capacity and copy the old elements to the new slice
+	fmt.Println(scores)
+
 }
