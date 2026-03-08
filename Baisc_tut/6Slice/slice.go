ff83cf5bafc811048dd0ebb3665de7efb9f79e17
srajalingam <rajalingam299@gmail.com>
2026-03-09 00:04:48 +0530

    slice example

--- Baisc_tut/6Slice/slice.go
+++ Baisc_tut/6Slice/slice.go
@@ -16,4 +16,26 @@ func main() {
 	numbers[2] = 10
 	fmt.Println("After modifying the original slice:", numbers)
 	fmt.Println("Subset after modifying the original slice:", subset)
+
+	//cap
+
+	hobbies := []string{"Reading", "Traveling", "Cooking"}
+	fmt.Println("Original hobbies slice:", hobbies)
+	fmt.Printf("Length: %d, Capacity: %d\n", len(hobbies), cap(hobbies))
+
+	mainHobbies := hobbies[:2]
+	fmt.Println("Main hobbies slice:2", hobbies)
+	fmt.Println("Main hobbies slice:", mainHobbies)
+	fmt.Printf("Length: %d, Capacity: %d\n", len(mainHobbies), cap(mainHobbies))
+
+	hobbies = hobbies[:2]
+	fmt.Println("Hobbies slice after slicing to main hobbies:", hobbies)
+	fmt.Printf("Length: %d, Capacity: %d\n", len(hobbies), cap(hobbies))
+
+	disccountPrices := []float64{19.99, 29.99, 39.99, 49.99}
+	prices := []float64{10, 20.0}
+
+	prices = append(prices, disccountPrices...)
+	fmt.Println("Prices after appending discount prices:", prices)
+
 }
