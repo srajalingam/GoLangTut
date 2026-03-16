34f000d5e24fe73feacfc04172e5042ca655f133
srajalingam <rajalingam299@gmail.com>
2026-03-17 00:11:35 +0530

    channel to get all the value print

--- GOroutine/goroutine.go
+++ GOroutine/goroutine.go
@@ -9,14 +9,18 @@ func greet(val string) {
 	fmt.Println("hi", val)
 }
 
-func slowGreet(val string) {
+func slowGreet(val string, doneChan chan bool) {
 	time.Sleep(3 * time.Second)
 	fmt.Println("hello", val)
+	doneChan <- true // <- pushing the value to channel
 }
 
 func main() {
 	go greet("Nice to meet you")
 	go greet("how are you")
-	go slowGreet("How .are ....you")
+	done := make(chan bool)
+	go slowGreet("How .are ....you", done)
+
 	go greet("hope you reached..")
+	<-done // is done
 }
