f37479eb2ba9a988f4f2a40f384d50794edda63a
srajalingam <rajalingam299@gmail.com>
2026-03-17 00:29:25 +0530

    2- longer process we can define close to mark process is done

--- GOroutine/goroutine.go
+++ GOroutine/goroutine.go
@@ -14,24 +14,29 @@ func slowGreet(val string, doneChan chan bool) {
 	time.Sleep(3 * time.Second)
 	fmt.Println("hello", val)
 	doneChan <- true // <- pushing the value to channel
+	close(doneChan)
 }
 
 func main() {
-	//done := make(chan bool)
-	dones := make([]chan bool, 4)
-	dones[0] = make(chan bool)
-	go greet("Nice to meet you", dones[0])
-	dones[1] = make(chan bool)
-	go greet("how are you", dones[1])
-	dones[2] = make(chan bool)
-	go slowGreet("How .are ....you", dones[2])
-	dones[3] = make(chan bool)
-	go greet("hope you reached..", dones[3])
+	done := make(chan bool)
+	//dones := make([]chan bool, 4)
+	// dones[0] = make(chan bool)
+	go greet("Nice to meet you", done)
+	// dones[1] = make(chan bool)
+	go greet("how are you", done)
+	// dones[2] = make(chan bool)
+	go slowGreet("How .are ....you", done)
+	// dones[3] = make(chan bool)
+	go greet("hope you reached..", done)
 	// <-done // is done
 	// <-done
 	// <-done
 	// <-done
-	for _, done := range dones {
-		<-done
+	// for _, done := range dones {
+	// 	<-done
+	// }
+
+	for range done {
+
 	}
 }
