be83ecf5a9d4c85441362735dfecd877521c8239
srajalingam <rajalingam299@gmail.com>
2026-03-28 01:28:37 +0530

    verify token function created

--- CRUD_GO/utils/jwt.go
+++ CRUD_GO/utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -15,4 +16,33 @@ func GenerateToken(email string, userId int64) (string, error) {
 		"exp":    time.Now().Add(time.Hour * 2).Unix(),
 	})
 	return token.SignedString([]byte(secertKey))
+}
+
+func VerifyToken(tokenString string) error {
+	parsedToken, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("Unexpected sigining method")
+		}
+		return []byte(secertKey), nil
+	})
+
+	if err != nil {
+		return errors.New("Could not parse token")
+	}
+	tokenIsValid := parsedToken.Valid
+
+	if !tokenIsValid {
+		return errors.New("Invalid token!")
+	}
+
+	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+
+	// if !ok {
+	// 	return errors.New("Invalid token claims.")
+	// }
+	// email := claims["email"].(string)
+	// userId := claims["userId"].(int64)
+
+	return nil
 }
